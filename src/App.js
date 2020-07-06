b36e0345f9db98fa00f31bc455f28b89822b6fa6
Laís Rhianne <lays.rhianne@gmail.com>
2020-07-06 20:57:39 -0300

    containers added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import './App.css';
+import { Container, Col, Row } from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Container className='themed-container' fluid={true} style={styles.container}>
+        <Row>
+          <Col sm='3' style={styles.monitoramento}>Teste1</Col>
+          <Col sm='6' style={styles.main}>Teste2</Col>
+          {/* <Col sm='2' style={styles.monitoramento}>Teste3</Col> */}
+        </Row>
+      </Container>
     </div>
   );
+}
+
+const styles = {
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  monitoramento: {
+    backgroundColor: 'blue',
+    marginRight: 30,
+    marginLeft: 'auto',
+    borderRadius: 10,
+  },
+  main: {
+    backgroundColor: 'yellow',
+    marginRight: 'auto',
+    marginLeft: 30,
+    borderRadius: 10,
+  }
 }
 
 export default App;
