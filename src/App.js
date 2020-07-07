5c7c57e442fbe23ec414869e312c48bffd80cc2d
Laís Rhianne <lays.rhianne@gmail.com>
2020-07-06 21:13:26 -0300

    style added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,27 @@ import { Container, Col, Row } from 'reactstrap';
 function App() {
   return (
     <div className='App'>
-      <Container className='themed-container' fluid={true} style={styles.container}>
+      <Container
+        className='themed-container'
+        fluid={true}
+        style={styles.container}
+      >
         <Row>
-          <Col sm='3' style={styles.monitoramento}>Teste1</Col>
-          <Col sm='6' style={styles.main}>Teste2</Col>
-          {/* <Col sm='2' style={styles.monitoramento}>Teste3</Col> */}
+          <Col sm='3' style={styles.monitoramento}>
+            Monitoramento
+            <br />
+            Montante atual
+            <br />
+            Gastos no mês
+            <br />
+          </Col>
+          <Col sm='6' style={styles.main}>
+            Finanças
+            <br />
+            Adicionar crédito/débito
+            <br />
+            Histórico
+          </Col>
         </Row>
       </Container>
     </div>
@@ -18,21 +34,33 @@ function App() {
 
 const styles = {
   container: {
+    backgroundColor: '#282c34',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    height: 657,
   },
   monitoramento: {
-    backgroundColor: 'blue',
+    backgroundColor: '#6DC5C7',
+    justifyContent: 'center',
+    alignItems: 'center',
     marginRight: 30,
     marginLeft: 'auto',
+    marginTop: 50,
+    marginBottom: 50,
+    paddingTop: 20,
+    paddingBottom: 20,
     borderRadius: 10,
   },
   main: {
-    backgroundColor: 'yellow',
+    backgroundColor: '#C78F6D',
     marginRight: 'auto',
     marginLeft: 30,
+    marginTop: 50,
+    marginBottom: 50,
+    paddingTop: 20,
+    paddingBottom: 20,
     borderRadius: 10,
-  }
-}
+  },
+};
 
 export default App;
